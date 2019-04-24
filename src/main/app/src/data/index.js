3ed62aa1df0b8d20bb276beba3561d6596135471
Shyam Kumar <shyam.gumudavelly@gmail.com>
2019-04-24 16:24:59 -0700

    test

--- src/main/app/src/data/index.js
+++ src/main/app/src/data/index.js
@@ -4,14 +4,12 @@ import type { Dispatch } from "redux";
 import { routerReducer } from "react-router-redux";
 
 import auth from "./modules/auth";
-import books from "./modules/books";
 import registrationItems from "./modules/equipment";
 import websockets from "./modules/websockets";
 
 const rootReducer = combineReducers({
   router: routerReducer,
   auth,
-  books,
   registrationItems,
   websockets
 });
